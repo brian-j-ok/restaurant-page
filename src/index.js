c4d4dbc6ae00384af12775b4032b252eeef7e9ec
brianjrok <brianjrok@gmail.com>
2022-02-16 15:04:20 -0500

    set up HtmlWebpackPlugin and index.html deletion issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './style.css';
 import Background from './cafe-background.gif';
 
-const content = document.getElementById('content');
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
 
 const myBackground = new Image();
 myBackground.src = Background;
 
 content.appendChild(myBackground);
+
+document.body.appendChild(content);
