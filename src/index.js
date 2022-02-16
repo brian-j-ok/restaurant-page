ee0a4373078f46a480fc4dce3d73647c0fe165d1
brianjrok <brianjrok@gmail.com>
2022-02-16 14:56:30 -0500

    set up background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import Background from './cafe-background.gif';
+
+const content = document.getElementById('content');
+
+const myBackground = new Image();
+myBackground.src = Background;
+
+content.appendChild(myBackground);
